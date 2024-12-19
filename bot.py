298fd3789b907bfde96ca66bb12ee9169a75b903
justarav <hazeifygameplay5927@gmail.com>
2024-12-19 12:55:44 +0530

    fixing auth error

--- bot.py
+++ bot.py
@@ -10,9 +10,9 @@ from telegram.ext import (
 from dotenv import load_dotenv
 
 load_dotenv()
-TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"]
-API_BASE_URL = os.environ["API_BASE_URL"]
-API_TOKEN = os.environ["API_TOKEN"]
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+API_BASE_URL = os.getenv('API_BASE_URL')
+API_TOKEN = os.getenv('API_TOKEN')
 
 default_keys = [
     {
